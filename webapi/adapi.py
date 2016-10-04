c85adcebcfc84074ba27b2bd9e24ec4a0eb3ac1a
Sean Whalen <whalenster@gmail.com>
2016-10-04 16:29:05 -0400

    Fix API decorator

--- webapi/adapi.py
+++ webapi/adapi.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from functools import wraps
 from ldap import LDAPError
 
 from easyad import EasyAD
@@ -14,6 +15,7 @@ def parse_ldap_error(e):
 
 
 def api_call(function):
+    @wraps(function)
     def process_api_call(*args, **kwargs):
         try:
             function(*args, **kwargs)
@@ -31,60 +33,40 @@ def index():
     return "Hello world!"
 
 
-@api_call
 @app.route("/user/<user_string>")
+@api_call
 def get_user(user_string):
     return jsonify(ad.get_user(user_string, json_safe=True))
 
 
 @app.route("/user/<user_string>/groups")
+@api_call
 def user_groups(user_string):
-    try:
-        return jsonify(ad.get_all_user_groups(user_string, json_safe=True))
-    except ValueError as e:
-        return jsonify(dict(error=str(e))), 404
-    except LDAPError as e:
-        return jsonify(dict(error=parse_ldap_error(e))), 500
+    return jsonify(ad.get_all_user_groups(user_string, json_safe=True))
 
 
 @app.route("/user/<user_string>/member-of/<group_string>")
+@api_call
 def user_is_member_of_group(user_string, group_string):
-    try:
-        return jsonify(dict(member=ad.user_is_member_of_group(user_string, group_string)))
-    except ValueError as e:
-        return jsonify(dict(error=str(e))), 404
-    except LDAPError as e:
-        return jsonify(dict(error=parse_ldap_error(e))), 500
+    return jsonify(dict(member=ad.user_is_member_of_group(user_string, group_string)))
 
 
 @app.route("/group/<group_string>")
+@api_call
 def get_group(group_string):
-    try:
-        return jsonify(ad.get_group(group_string, json_safe=True))
-    except ValueError as e:
-        return jsonify(dict(error=str(e))), 404
-    except LDAPError as e:
-        return jsonify(dict(error=parse_ldap_error(e))), 500
+    return jsonify(ad.get_group(group_string, json_safe=True))
 
 
 @app.route("/group/<group_string>/members")
+@api_call
 def get_group_members(group_string):
-    try:
-        return jsonify(ad.get_all_users_in_group(group_string, json_safe=True))
-    except ValueError as e:
-        return jsonify(dict(error=str(e))), 404
-    except LDAPError as e:
-        return jsonify(dict(error=parse_ldap_error(e))), 500
+    return jsonify(ad.get_all_users_in_group(group_string, json_safe=True))
 
 
 @app.route("/search/user/<user_string>")
+@api_call
 def search_for_users(user_string):
-    try:
-        return jsonify(ad.search_for_users(user_string, json_safe=True))
-    except ValueError as e:
-        return jsonify(dict(error=str(e))), 404
-    except LDAPError as e:
-        return jsonify(dict(error=parse_ldap_error(e))), 500
+    return jsonify(ad.search_for_users(user_string, json_safe=True))
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
